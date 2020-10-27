23f80a3eadcee3b2317edf68e3ec37fbfc2a8103
Taiyo2406 <zm19029@josai.ac.jp>
2020-10-27 02:00:45 +0000

    1027

--- ex6-5.js
+++ ex6-5.js
@@ -1,14 +1,18 @@
 var who = 'world';
-var come = ' ';
+//var come = ' ';
 function sayhello65() {
-alert('Hello, ' + who + come +'!');
+alert('Hello, ' + who  +'!');
 }
 
 function taro65() {
 who = '太郎';
-come = 'が来た';
+//come = 'が来た';
 }
 function hanako65() {
 who = '花子';
-come = 'が来た';
+//come = 'が来た';
+}
+ function gakita() {
+'が来た';
+
 }
