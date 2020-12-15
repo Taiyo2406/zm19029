c82e94fb4cf6d7abbf71bffb08bd60b1a789fd09
Taiyo2406 <zm19029@josai.ac.jp>
2020-12-15 01:38:32 +0000

    message

--- ex06-10.js
+++ ex06-10.js
@@ -2,3 +2,7 @@ function imgchange01() {
 document.getElementById('logo01').src =
 './image/crystal.gif';
 }
+function imgchange02() {
+document.getElementById('logo01').src =
+'./image/keiei_small.jpg';
+}
